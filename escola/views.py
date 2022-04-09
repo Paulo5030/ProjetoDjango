f43037105502bf14d8ced35cedf5b96a3f08a23e
Paulo Henrique <paulohenrique20012@hotmail.com>
2022-04-09 13:30:31 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,45 @@
+from telnetlib import AUTHENTICATION
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoserializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os Alunos e Alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os Cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MatriculaViewset(viewsets.ModelViewSet):
+    """Listando todas as Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as Matriculas de um Aluno ou Aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoserializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando Alunos e Alunas Matriculados em um Curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
